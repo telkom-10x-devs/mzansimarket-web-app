82cebb5f4e71238699212d70fc2dff5e9040b667
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-08-17 10:36:22 +0200

    fix: static link handling

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Configure for your frontend URL
+    origin: process.env.FRONTEND_URL, // Configure for your frontend URL
     credentials: true,
   })
 );
